d65fb8e984de7dc3e32d0b53fb71d764bd22540b
NanneStephan <31951931+NanneStephan@users.noreply.github.com>
2018-09-02 15:14:17 +0200

    minor update Async

--- sample/src/main/java/org/stellar/android/sample/MainActivity.java
+++ sample/src/main/java/org/stellar/android/sample/MainActivity.java
@@ -2,52 +2,37 @@ package org.stellar.android.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.TextView;
 
 import org.stellar.sdk.KeyPair;
-import org.stellar.sdk.Server;
 import org.stellar.sdk.responses.AccountResponse;
-import org.stellar.sdk.responses.HttpResponseException;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Stellar";
+    public static KeyPair pair;
+    public static AccountResponse.Balance balance;
+
+    String strBalance;
+
+    TextView successTextView;
+    TextView balanceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        successTextView = findViewById(R.id.Success);
+        balanceTextView = findViewById(R.id.Balance);
 
-        new Thread() {
-            @Override
-            public void run() {
+        String secretKey = "SAO6SPWEBUHXKMDOPZ4JZTETXC4H4ZTLJYWXQCGR6WYN2DCDR26LTK3I";
+        String publicKey = "GAZQJ4VPNWBUMAD7MZM3XPO5QUJEEKIJZMXKXNUIX7NBLZVTR5SCUKGI";
+        pair = KeyPair.fromSecretSeed(secretKey);
+        successTextView.setText("Balances for account " + pair.getAccountId());
+        balanceTextView.setText(strBalance);
 
-                // Generate ok https://www.stellar.org/laboratory/#account-creator?network=test
-                String publicKey = "GAZQJ4VPNWBUMAD7MZM3XPO5QUJEEKIJZMXKXNUIX7NBLZVTR5SCUKGI";
-                String secretKey = "SAO6SPWEBUHXKMDOPZ4JZTETXC4H4ZTLJYWXQCGR6WYN2DCDR26LTK3I";
-                KeyPair pair = KeyPair.fromSecretSeed(secretKey);
+        new StellarAsync().execute();
 
-                Server server = new Server("https://horizon-testnet.stellar.org");
 
-                AccountResponse account = null;
-                try {
-                    account = server.accounts().account(pair);
-                    Log.i(TAG, "Balances for account " + pair.getAccountId());
-                    for (AccountResponse.Balance balance : account.getBalances()) {
-                        Log.i(TAG, String.format(
-                                "Type: %s, Code: %s, Balance: %s",
-                                balance.getAssetType(),
-                                balance.getAssetCode(),
-                                balance.getBalance()));
-                    }
-                } catch (HttpResponseException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
     }
 }
---
+++ sample/src/main/java/org/stellar/android/sample/StellarAsync.java
@@ -0,0 +1,43 @@
+package org.stellar.android.sample;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.stellar.sdk.KeyPair;
+import org.stellar.sdk.Server;
+import org.stellar.sdk.responses.AccountResponse;
+import org.stellar.sdk.responses.HttpResponseException;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.stellar.android.sample.MainActivity.TAG;
+
+public class StellarAsync extends AsyncTask<URL, String, String> {
+    @Override
+    protected String doInBackground(URL... urls) {
+        String publicKey = "GAZQJ4VPNWBUMAD7MZM3XPO5QUJEEKIJZMXKXNUIX7NBLZVTR5SCUKGI";
+        String secretKey = "SAO6SPWEBUHXKMDOPZ4JZTETXC4H4ZTLJYWXQCGR6WYN2DCDR26LTK3I";
+        KeyPair pair = KeyPair.fromSecretSeed(secretKey);
+
+        Server server = new Server("https://horizon-testnet.stellar.org");
+
+        AccountResponse account = null;
+        try {
+            account = server.accounts().account(pair);
+            Log.i(TAG, "Balances for account " + pair.getAccountId());
+            for (AccountResponse.Balance balance : account.getBalances()) {
+                Log.i(TAG, String.format(
+                        "Type: %s, Code: %s, Balance: %s",
+                        balance.getAssetType(),
+                        balance.getAssetCode(),
+                        balance.getBalance()));
+            }
+        } catch (HttpResponseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
